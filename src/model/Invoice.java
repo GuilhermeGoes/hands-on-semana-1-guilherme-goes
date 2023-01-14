773de41c37fab780735b5cb2ad5f45f3842a86cf
GuilhermeGoes <guilhermegoes76@hotmail.com>
2023-01-14 10:35:41 -0300

    FEAT(Invoice): criando entidade de fatura

---
+++ src/model/Invoice.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Invoice {
+
+    private String company;
+    private int month;
+    private int year;
+    private String firstInstallmentDate;
+    private float firstInstallmente;
+    private String secondInstallmentDate;
+    private float secondInstallmente;
+    private String thirdInstallmentDate;
+    private float thirdInstallmente;
+
+
+}

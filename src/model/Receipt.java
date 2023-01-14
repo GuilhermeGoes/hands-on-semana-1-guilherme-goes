d14de0dc0695a94e85a9fd23bef223941d02663b
GuilhermeGoes <guilhermegoes76@hotmail.com>
2023-01-14 10:36:19 -0300

    FEAT(Receipt): criando entidade de notas

---
+++ src/model/Receipt.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Receipt {
+
+    private String company;
+    private int month;
+    private int year;
+    private float value;
+    private String issueDate;
+    private int bill;
+
+}
